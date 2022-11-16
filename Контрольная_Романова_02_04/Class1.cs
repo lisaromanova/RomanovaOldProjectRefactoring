d9a82d6239d505efde05ec55880e9d266775261b
Elizaveta <soika-peresmeshnica2015@yandex.ru>
2022-11-16 23:11:37 +0300

    Добавьте файлы проекта.

---
+++ Контрольная_Романова_02_04/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Контрольная_Романова_02_04
+{
+    interface Location
+    {
+        void show();
+        int shop();
+        void hospital();
+    }
+    class City : Location
+    {
+        public int kol; //количество магазинов в одном райное
+        public int area; //количество районов в городе
+        string city; // название города
+        public int child; //количество детских больниц в районе
+        public int adult; //количество взрослых боьниц в районе
+        public City(string city)
+        {
+            this.city = city;
+        }
+        public virtual void show() // вывод города в котором живете
+        {
+            Console.WriteLine("Вы живете в городе " + city);
+        }
+        public int shop() //подсчет количества магазинов в городе по количеству магазинов в одном районе (в каждом районе одинаковое количество магазинов)
+        {
+            return area * kol;
+        }
+        public void hospital() //количество детских и взрослых больниц в городе
+        {
+            Console.WriteLine($"В городе {child*area} детских больниц и {adult*area} взрослых");
+        }
+    }
+    class Village: City
+    {
+        string village; //название деревни
+        public int residents_new; //население деревни этого года
+        public int residents_old;//население деревни прошлого года
+        public Village(string village, string city):base(city)
+        {
+            this.village = village;
+        }
+        public override void show() //вывод города и деревни обитания
+        {
+            base.show();
+            Console.WriteLine("Вы живете в деревне "+ village);
+        }
+        public void Population() //вывод прибыли и убыли населения
+        {
+            if (residents_new > residents_old)
+            {
+                Console.WriteLine($"Прибыль населения на {residents_new-residents_old} человека");
+            }
+            else
+            {
+                if(residents_new < residents_old)
+                {
+                    Console.WriteLine($"Убыль населения на {residents_old - residents_new} человека");
+                }
+                else
+                {
+                    Console.WriteLine("Численность населения не изменилась");
+                }
+            }
+            
+        }
+        new public void hospital() //вывод количества больниц в деревне
+        {
+            Console.WriteLine($"В деревне всего {child + adult} больниц");
+        }
+    }
+}
---
+++ Контрольная_Романова_02_04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Контрольная_Романова_02_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите город");
+            string city = Console.ReadLine();
+            City A = new City(city);
+            A.show();
+            Console.WriteLine("Введите количество районов в городе");
+            A.area = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество магазинов в одном районе города");
+            A.kol = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"В городе {A.shop()} магазинов 'Пятерочка'");
+            Console.WriteLine("Введите количество детских больниц в районе города");
+            A.child = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество взрослых больниц в районе города");
+            A.adult = Convert.ToInt32(Console.ReadLine());
+            A.hospital();
+            Console.WriteLine("Введите название деревни");
+            string village = Console.ReadLine();
+            Village B = new Village(village, city);
+            B.show();
+            Console.WriteLine("Введите население деревни в этом году");
+            B.residents_new = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите население деревни в прошлом году");
+            B.residents_old = Convert.ToInt32(Console.ReadLine());
+            B.Population();
+            Console.WriteLine("Введите количество детских больниц в деревне");
+            B.child = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество взрослых больниц города");
+            B.adult = Convert.ToInt32(Console.ReadLine());
+            B.hospital();
+        }
+    }
+}
